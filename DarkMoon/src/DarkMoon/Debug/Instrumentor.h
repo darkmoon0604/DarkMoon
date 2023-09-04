c267fafec2cf3f2487453d8aa9ea302272a001bf
三月 <327750124@qq.com>
2023-09-04 11:11:19 +0800

    跨平台宏定义

--- DarkMoon/src/DarkMoon/Debug/Instrumentor.h
+++ DarkMoon/src/DarkMoon/Debug/Instrumentor.h
@@ -126,10 +126,29 @@ namespace DarkMoon
 
 #define DM_PROFILE TRUE
 #if DM_PROFILE
+
+#if defined(__GNUC__) || defined(__MWERKS__) && (__MWERKS__ >= 0x30000) || defined(__ICC) && (__ICC >= 600) || defined(__ghs__)
+#define DM_FUNC_SIG __PRETTY_FUNCTION__
+#elif defined(__DMC__) && (__DMC__ >= 0x810)
+#define DM_FUNC_SIG __PRETTY_FUNCTION__
+#elif defined(__FUNCSIG__)
+#define DM_FUNC_SIG __FUNCSIG__
+#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+#define DM_FUNC_SIG __FUNCTION__
+#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+#define DM_FUNC_SIG __FUNC__
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+#define DM_FUNC_SIG __func__
+#elif defined(__cplusplus) && (__cplusplus >= 201103)
+#define DM_FUNC_SIG __func__
+#else
+#define DM_FUNC_SIG "DM_FUNC_SIG unknown!"
+#endif
+
 #define DM_PROFILE_BEGIN_SESSION(name, filePath) ::DarkMoon::Instrumentor::Get().BeginSession(name, filePath)
 #define DM_PROFILE_END_SESSION() ::DarkMoon::Instrumentor::Get().EndSession()
 #define DM_PROFILE_SCOPE(name) ::DarkMoon::InstrumentationTimer timer##__LINE__(name);
-#define DM_PROFILE_FUNCTION() DM_PROFILE_SCOPE(__FUNCSIG__)
+#define DM_PROFILE_FUNCTION() DM_PROFILE_SCOPE(DM_FUNC_SIG)
 #else
 #define DM_PROFILE_BEGIN_SESSION(name, filePath)
 #define DM_PROFILE_END_SESSION()
